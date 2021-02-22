a1957ea4beee8c4e77e3f5fedfbcbe1e2037154d
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-02-22 20:46:46 +0700

    sua js

--- JS/js.js
+++ JS/js.js
@@ -1,98 +1,131 @@
-window.onload=function currentTime() {
-    var date = new Date();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec;
-      var t = setTimeout(function(){ currentTime() }, 1000);
-      //1:44
-      //1:30
-/*-----------------cal1-------------------*/
-
-      document.getElementById("cal1").onclick= function calcutate1(){
-        var h=hour; var m=min; var i=1;
-        h+=1;
-        m+=44;
-        while(h>=24 || m>=60){
-          if(m>=60){
-            m-=60;
-            h++;}
-          if(h>=24){
-            h-=24;}
-          }
-        document.getElementById("show"+i).innerText=updateTime(h) + " : " + updateTime(m);
-        while(i<6){
-          i++;
-          h+=1;
-          m+=30;
-          while(h>=24 || m>=60){
-            if(m>=60){
-              m-=60;
-              h++;}
-            if(h>=24){
-              h-=24;}
-            }
-          document.getElementById("show"+i).innerText=updateTime(h) + " : " + updateTime(m);
-        }
-        toggle(1);
-      
-        
-      }
-/*-----------------cal2-------------------*/
-      document.getElementById("cal2").onclick= function calcutate2(){
-        var h; var m; var i=6;
-        var container =document.getElementById("time").value.split(":");
-        h=container[0];
-        m=container[1];
-        h-=1;
-        m-=44;
-          if(m<0){
-            h--;
-            m=60+m;
-          }
-          if(h<0){
-            h=12+h;
-        }
-        h=updateTime(h);
-        m=updateTime(m);
-        document.getElementById("show2_"+i).innerText=h + " : " + m;
-          while(i>1){
-              h-=1;
-              m-=30;
-              if(m<0){
-                h--;
-                m=60+m;
-              }
-              if(h<0){
-                  h=12+h;
-            }
-            h=updateTime(h);
-            m=updateTime(m);
-            i--;
-            document.getElementById("show2_"+i).innerText=h + " : " + m;
-            }
-          toggle(2);
-       
-
-      }
-
-  function updateTime(k) {
-    if (k < 10) {
-      return "0" + k;
-    }
-    else {
-      return k;
-    }
-  }
- 
+window.onload = function functions() {
+  currentTime();
+  document.getElementById("cal1").onclick = calcutate1();
+  document.getElementById("cal2").onclick = calcutate2();
+  document.getElementById("top").onclick = topFunction();
+  window.onscroll = function () {
+    scrollFunction()
+  };
 }
-function toggle(t){
-  if (document.getElementById("toggle"+t).style.display === "none") {
-    document.getElementById("toggle"+t).style.display = "block";
+/*functions*/
+//current time
+function currentTime() {
+  var date = new Date();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText = hour + " : " + min + " : " + sec;
+  var t = setTimeout(function () {
+    currentTime()
+  }, 1000);
+}
+//update time
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
   } else {
-    document.getElementById("toggle"+t).style.display = "none";
+    return k;
   }
+}
+//cal1
+function calcutate1() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var i = 1;
+  h += 1;
+  m += 44;
+  while (h >= 24 || m >= 60) {
+    if (m >= 60) {
+      m -= 60;
+      h++;
+    }
+    if (h >= 24) {
+      h -= 24;
+    }
+  }
+  document.getElementById("show" + i).innerText = updateTime(h) + " : " + updateTime(m);
+  while (i < 6) {
+    i++;
+    h += 1;
+    m += 30;
+    while (h >= 24 || m >= 60) {
+      if (m >= 60) {
+        m -= 60;
+        h++;
+      }
+      if (h >= 24) {
+        h -= 24;
+      }
+    }
+    document.getElementById("show" + i).innerText = updateTime(h) + " : " + updateTime(m);
+  }
+  toggle(1);
+
+
+}
+//cal2
+function calcutate2() {
+  var h;
+  var m;
+  var i = 6;
+  var container = document.getElementById("time").value.split(":");
+  h = container[0];
+  m = container[1];
+  h -= 1;
+  m -= 44;
+  if (m < 0) {
+    h--;
+    m = 60 + m;
+  }
+  if (h < 0) {
+    h = 12 + h;
+  }
+  h = updateTime(h);
+  m = updateTime(m);
+  document.getElementById("show2_" + i).innerText = h + " : " + m;
+  while (i > 1) {
+    h -= 1;
+    m -= 30;
+    if (m < 0) {
+      h--;
+      m = 60 + m;
+    }
+    if (h < 0) {
+      h = 12 + h;
+    }
+    h = updateTime(h);
+    m = updateTime(m);
+    i--;
+    document.getElementById("show2_" + i).innerText = h + " : " + m;
+  }
+  toggle(2);
+
+
+}
+//toggle
+function toggle(t) {
+  if (document.getElementById("toggle" + t).style.display === "none") {
+    document.getElementById("toggle" + t).style.display = "block";
+  } else {
+    document.getElementById("toggle" + t).style.display = "none";
+  }
+}
+//display go top button
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("top").style.display = "block";
+  } else {
+    document.getElementById("top").style.display = "none";
+  }
+}
+//go top
+function topFunction() {
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 }
